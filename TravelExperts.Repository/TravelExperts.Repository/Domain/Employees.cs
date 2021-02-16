e4145717b0b8a5e6831303645a81ceff746106a1
Holly Krause <holly.krause@edu.sait.ca>
2021-02-16 12:53:17 -0700

    starter

---
+++ TravelExperts.Repository/TravelExperts.Repository/Domain/Employees.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TravelExperts.Repository.Domain
+{
+    public partial class Employees
+    {
+        public string EmpFirstName { get; set; }
+        public string EmpMiddleInitial { get; set; }
+        public string EmpLastName { get; set; }
+        public string EmpBusPhone { get; set; }
+        public string EmpEmail { get; set; }
+        public string EmpPosition { get; set; }
+    }
+}
---
+++ TravelExperts.Repository/TravelExperts.Repository/Domain/SupsOld.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TravelExperts.Repository.Domain
+{
+    public partial class SupsOld
+    {
+        public int SupplierId { get; set; }
+        public string SupName { get; set; }
+    }
+}
