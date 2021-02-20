bf8ed4cd26f9abe30e1bcf00301f8e8dc934131c
ricobadico <biondiceric@gmail.com>
2021-02-20 09:08:27 -0700

    annotated customer entity for display

--- TravelExperts.Repository/TravelExperts.Repository/Domain/Customers.cs
+++ TravelExperts.Repository/TravelExperts.Repository/Domain/Customers.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace TravelExperts.Repository.Domain
 {
@@ -13,18 +14,30 @@ namespace TravelExperts.Repository.Domain
         }
 
         public int CustomerId { get; set; }
+        [Display(Name = "First Name")]
         public string CustFirstName { get; set; }
+        [Display(Name = "Last Name")]
         public string CustLastName { get; set; }
+        [Display(Name = "Address")]
         public string CustAddress { get; set; }
+        [Display(Name = "City")]
         public string CustCity { get; set; }
+        [Display(Name = "Province")]
         public string CustProv { get; set; }
+        [Display(Name = "Postal Code")]
         public string CustPostal { get; set; }
+        [Display(Name = "Country")]
         public string CustCountry { get; set; }
+        [Display(Name = "Home Phone")]
         public string CustHomePhone { get; set; }
+        [Display(Name = "Business Phone")]
         public string CustBusPhone { get; set; }
+        [Display(Name = "Email")]
         public string CustEmail { get; set; }
         public int? AgentId { get; set; }
+        [Display(Name = "Login")]
         public string UserLogin { get; set; }
+        [Display(Name = "Password")]
         public string UserPass { get; set; }
 
         public virtual Agents Agent { get; set; }
