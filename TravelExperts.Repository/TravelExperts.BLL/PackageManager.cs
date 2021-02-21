e25dfa8574ece9d39cba1154f1d2f51d6be61ea8
ricobadico <biondiceric@gmail.com>
2021-02-21 11:09:58 -0700

    working on recommendation code

--- TravelExperts.Repository/TravelExperts.BLL/PackageManager.cs
+++ TravelExperts.Repository/TravelExperts.BLL/PackageManager.cs
@@ -32,5 +32,29 @@ namespace TravelExperts.BLL
             return pack;
         }
 
+        public static IList GetRecommendations(int custID)
+        {
+            //open the DB
+            TravelExpertsContext db = new TravelExpertsContext();
+
+            // Get a list of all trips the customer has been on
+            // This is hard to quantify in the existing data, since no Packages have been ordered..
+            // As is, we'll use the Description field for booking details, which typically represents a whole booking
+
+            List<int> previousCustBookings = db.Bookings
+                .Where(booking => booking.CustomerId != null && booking.CustomerId == custID)
+                .Select(booking => booking.BookingId).ToList(); // taking only the current customer's records
+
+            List<string> previousCustTrips = db.BookingDetails
+                .Where(bookingDetail => bookingDetail.BookingId != null
+                    && previousCustBookings.Contains(Convert.ToInt32(bookingDetail.BookingId)))
+                .Select(detail => detail.Description)
+                .Distinct().ToList();
+
+            // TODO: Use the list of trips the customer has gone on, find other customers who have gone on them
+            // Then return the most popular trips among those
+
+            return new List<Bookings>();
+        }
     }
 }
