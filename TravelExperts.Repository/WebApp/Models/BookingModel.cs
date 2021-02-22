c52d2d28d1c10de750f0d5c6f726f49428cb2d48
Chris Eckstadt <Christopher.Eckstadt@edu.sait.ca>
2021-02-21 17:18:59 -0700

    Added viewing models to display all cutomer related booking information with booking costs

--- TravelExperts.Repository/WebApp/Models/BookingModel.cs
+++ TravelExperts.Repository/WebApp/Models/BookingModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using TravelExperts.Repository.Domain;
@@ -10,19 +11,37 @@ namespace TravelExpertsWebApp.Models
     {
 
         public int BookingId { get; set; }
+        [Display(Name ="Booking Date")]
         public DateTime? BookingDate { get; set; }
+        [Display(Name = "Booking Number")]
         public string BookingNo { get; set; }
+        [Display(Name = "Traveler Count")]
         public double? TravelerCount { get; set; }
         public int? CustomerId { get; set; }
         public string TripTypeId { get; set; }
         public int? PackageId { get; set; }
-
+        [Display(Name = "Itinerary Number")]
         public string ItineraryNo { get; set; }
+        [Display(Name = "Trip Start Date")]
         public string TripStart { get; set; }
+        [Display(Name = "Trip End Date")]
         public string TripEnd { get; set; }
         public string Description { get; set; }
         public string Destination { get; set; }
-        public decimal? BasePrice { get; set; }
+        [Display(Name = "Base Pice")]
+        public decimal? BasePrice { get {
+                decimal? sum = 0;
+                foreach (BookingDetails bd in BookingDetails)
+                {
+                    if (bd.BasePrice != null)
+                    {
+                        //Get fee totals for all booking details
+                        sum += bd.Fee.FeeAmt;
+                        sum += bd.BasePrice;
+                    }
+                }
+                return sum;
+            } set { BasePrice = value; } }
 
         public virtual Customer Customer { get; set; }
         public virtual Packages Package { get; set; }
@@ -30,6 +49,7 @@ namespace TravelExpertsWebApp.Models
         public virtual Classes Class { get; set; }
         public virtual Fees Fee { get; set; }
         public virtual Regions Region { get; set; }
+        public virtual ICollection<BookingDetails> BookingDetails { get; set; }
 
     }
 }
